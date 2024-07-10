5e5ed74c4e86829a1d08c682054c93b0ad7631ec
Rob Knight <raknight@ucsc.edu>
2024-07-10 12:29:02 -0700

    docs: 📝 Comment and docblock syntax clean up

--- lib/functions/general.php
+++ lib/functions/general.php
@@ -11,28 +11,20 @@
  * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
 
-if ( ! function_exists( 'ucsc_news_enqueue_admin_styles' ) ) {
-	/**
-	* Enqueue admin settings styles
-	*
-	* No styles are enqueued for raw HTML in setting panel.
-	* In order to output HTML in the settings panel we need some basic styles.
-	*
-	* @since 0.1.0
-	* @author UCSC
-	* @link https://developer.wordpress.org/reference/hooks/admin_enqueue_scripts/#Example:_Load_CSS_File_from_a_plugin_on_specific_Admin_Page
-	*
-	*/
 
+/**
+*	Enqueue admin settings styles
+*/ 
+if ( ! function_exists( 'ucsc_news_enqueue_admin_styles' ) ) {
+	
 	function ucsc_news_enqueue_admin_styles($hook){
 		$settings_css = plugin_dir_url(__FILE__) . 'lib/css/admin-settings.css';
 		$current_screen = get_current_screen();
+		
 		// Check if it's "?page=ucsc-news-functionality-settings." If not, just empty return. 
 		if ( strpos($current_screen->base, 'ucsc-news-functionality-settings') === false) {
 			return;
 		} else {
-
-			// Load css.
 			wp_register_style( 'ucsc-news-admin-settings', $settings_css,);
 			wp_enqueue_style( 'ucsc-news-admin-settings');
 		}
@@ -63,12 +55,10 @@ if ( ! function_exists( 'ucsc_news_functionality_plugin_action_links' ) ){
 		return $links;
 	}
 }
-/*
- *  Filter Media Coverage posts
- *  Set permalinks to the external URL
- *  
- */
 
+/**
+*	Set permalinks to the external URL  
+*/
 if ( ! function_exists( 'ucsc_news_link_filter' ) ){
 	function ucsc_news_link_filter($post_link, $post) {
 		if ( ( 'media_coverage' === $post->post_type ) ) {
@@ -84,13 +74,9 @@ if ( ! function_exists( 'ucsc_news_link_filter' ) ){
 
 add_filter('post_type_link', 'ucsc_news_link_filter', 10, 2);
 
-/*
- *  Filter Media Coverage posts
- *  Replace default placeholder value
- *  
- */
-
-
+/**
+*	Replace default placeholder value
+*/
 if ( ! function_exists( 'ucsc_news_change_media_coverage_title_placeholder' ) ){
 	function ucsc_news_change_media_coverage_title_placeholder( $title ){
 		$screen = get_current_screen();
